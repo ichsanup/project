25f1d33504fa21c066e166cfa8867300451277ac
ichsanup <ichsanup@gmail.com>
2024-09-20 22:01:40 +0700

    first commit

---
+++ Object Repository/Login_Positive/b_Signed in successfully.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>b_Signed in successfully</name>
+   <tag></tag>
+   <elementGuidId>9de18568-150b-461f-bc56-ff95d510ec00</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//b[@id='flash_notice']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#flash_notice</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>b</value>
+      <webElementGuid>c1b472fb-d331-493d-97b9-25113509a7b1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>flash_notice</value>
+      <webElementGuid>2d659360-aba4-4faa-aa62-f66b49fff725</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Signed in successfully.</value>
+      <webElementGuid>e5bcad87-6ce4-4800-8f81-70fc8cee9de6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;flash_notice&quot;)</value>
+      <webElementGuid>7c73aff9-ab6d-4277-b86c-61b9a71d1d4b</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//b[@id='flash_notice']</value>
+      <webElementGuid>fbbc487f-3bdd-4dbe-9ed6-b4020c024159</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='notify']/b</value>
+      <webElementGuid>7333e869-f475-4eaa-86fa-920ce1a793a9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Reminders In Next 30 Days'])[1]/following::b[1]</value>
+      <webElementGuid>e6daf54d-f887-4fcf-aa63-a85b4313d220</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='No upcoming reminders.'])[1]/following::b[1]</value>
+      <webElementGuid>2ad07aba-a8e2-4a3d-bda7-5332971c2a2a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Document is being processed, check progress on process log!'])[1]/preceding::b[1]</value>
+      <webElementGuid>f9b6fcd8-5533-42b9-b812-3f150814d1c9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Signed in successfully.']/parent::*</value>
+      <webElementGuid>5753bdbc-eece-42d7-85e1-bec9f270ca29</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div/b</value>
+      <webElementGuid>81e2b749-b6d6-476d-bcb1-b00e94d1ea71</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//b[@id = 'flash_notice' and (text() = 'Signed in successfully.' or . = 'Signed in successfully.')]</value>
+      <webElementGuid>2eab12ea-8ea0-4bed-a44c-fb881d42649a</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
